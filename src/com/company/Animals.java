93c0fd59051a868ebf120925404c43befd6a8c2e
yukiaire <yukiaire@rambler.ru>
2020-04-03 21:00:08 +0200

    FirstBranch

--- src/com/company/Animals.java
+++ src/com/company/Animals.java
@@ -2,12 +2,13 @@ package com.company;
 
 public abstract class Animals {
     String Name, Breed;
-    int Age;
+    int Age, lifeYears;
 
     public Animals (String Name, String Breed, int Age) {
         this.Name = Name;
         this.Breed = Breed;
         this.Age = Age;
+        this.lifeYears = 5;
     }
 
     public void ShowName() {
